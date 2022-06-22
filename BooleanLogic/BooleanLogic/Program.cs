5937d3d80d18388e93b566c354e3aa3af6a4483c
H-Grayson <92130199+H-Grayson@users.noreply.github.com>
2022-06-22 07:59:42 -0600

    CarInsuranceApproval

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // && and operator and/and
+            Console.WriteLine(true && false);
+            Console.WriteLine(true && true);
+            Console.WriteLine(false && false);
+
+            // || or operator true or true
+            Console.WriteLine(true || true);
+            Console.WriteLine(true || false);
+            Console.WriteLine(false || false);
+
+            // == equal to
+            Console.WriteLine(true == true);
+            Console.WriteLine(true == false);
+            Console.WriteLine(false == false);
+
+            //  != not equal to
+            Console.WriteLine(true != true);
+            Console.WriteLine(true != false);
+            Console.WriteLine(false != false);
+
+            // ^ XOR, one or another is true, but not both
+            Console.WriteLine(true ^ true);
+            Console.WriteLine(true ^ false);
+            Console.WriteLine(false ^ false);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ CarInsuranceApproval/CarInsuranceApproval/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsuranceApproval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? Please state True or False.");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int spdTckt = Convert.ToInt32(Console.ReadLine());
+
+            bool ageQual = age > 15;
+            bool duiQual = dui == false;
+            bool spdTcktQual = spdTckt < 3;
+
+            bool insQual = ageQual == duiQual == spdTcktQual == true;
+
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(insQual);
+            Console.ReadLine();
+        }
+    }
+}
