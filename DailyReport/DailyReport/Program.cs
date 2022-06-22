df1475e79bd590fefa362df61479eef59e313395
H-Grayson <92130199+H-Grayson@users.noreply.github.com>
2022-06-22 04:27:56 -0600

    Daily Report
    Bare minimum, not sure what else I was supposed to do.

---
+++ DailyReport/DailyReport/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy\n");
+            
+            Console.WriteLine("Student Daily Report");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your name?");
+            string yourname = Console.ReadLine();
+
+            Console.WriteLine("\nWhat course are you on?");
+            string coursename = Console.ReadLine();
+
+            Console.WriteLine("\nWhat page number?");
+            string pagenum = Console.ReadLine();
+            int pgNum = Convert.ToInt32(pagenum);
+
+            Console.WriteLine("\nDo you need help with anything? Please answer “true” or “false.”");
+            string needhelp = Console.ReadLine();
+            bool ndhelp = Convert.ToBoolean(needhelp);
+
+            Console.WriteLine("\nWere there any positive experiences you’d like to share? Please give specifics.");
+            string experiences = Console.ReadLine();
+
+            Console.WriteLine("\nIs there any other feedback you’d like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+
+            Console.WriteLine("\nHow many hours did you study today?");
+            string studyhrs = Console.ReadLine();
+            int stdyhrs = Convert.ToInt32(studyhrs);
+
+            Console.WriteLine("\nThank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+        }
+    }
+}
---
+++ VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is a comment, you can use a comment to disinclude code for testing
+            // Use ctrl + k + c to comment a block of code, ctrl + k + u to undo
+            //Console.WriteLine("What is your name?");
+            //string yourname = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourname);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plust 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            char questionMark = '\u2103';
+
+
+            decimal moneyInBank = 100.5m; // m = decimal
+
+            double heightInCentimeters = 211.30202092;
+
+            float secondsLeft = 2.62f; //f = float;
+
+            short temperatureOnMars = -341;
+
+            string myName = "Hunter";
+
+            //integer to a string
+            int currentAge = 26;
+            string yearsOld = currentAge.ToString();
+
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+            
+        }
+    }
+}
