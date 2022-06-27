ca0eea95ded7ac155b534dd18cde0457d01b5ef7
H-Grayson <92130199+H-Grayson@users.noreply.github.com>
2022-06-27 05:14:30 -0600

    Main Method Assignment

---
+++ MainMethodAssignment/MainMethodAssignment/Methods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class Methods
+    {
+        public static int Math(int Num)
+        {
+            int result = Num + 10;
+            return result;
+        }
+
+        public static decimal Math(decimal Num)
+        {
+            int result = (int)(Num * 10);
+            return result;
+        }
+
+        public static int Math(string Num)
+        {
+            int NumConv = Convert.ToInt32(Num);
+            int result = (NumConv / 5);
+            return result;
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("25 + 10 =  " + Methods.Math(25));
+
+            Console.WriteLine("3.5 * 10 =   " + Methods.Math(3.5m));
+
+            Console.WriteLine("25 / 5 =   " + Methods.Math("25"));
+
+            Console.ReadLine();
+        }
+    }
+}
