cb897cd9bf28b12e8b8af2d5ecbce5b8f71d0a64
H-Grayson <92130199+H-Grayson@users.noreply.github.com>
2022-06-27 04:50:27 -0600

    CallMethods

---
+++ CallMethodsAssignment/CallMethodsAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a number for math operations to be used on.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(userNum + " + 5 = " + methods.Addition(userNum));
+
+            Console.WriteLine(userNum + " - 5 = " + methods.Subtraction(userNum));
+
+            Console.WriteLine(userNum + " / 2 = " + methods.Division(userNum));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CallMethodsAssignment/CallMethodsAssignment/methods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallMethodsAssignment
+{
+    public class methods
+    {
+        public static int Addition(int userNum)
+        {
+            int add = userNum + 5;
+            return add;
+        }
+        public static int Subtraction(int userNum)
+        {
+            int subtract = userNum - 5;
+            return subtract;
+        }
+        public static int Division(int userNum)
+        {
+            int divide = userNum / 2;
+            return divide;
+        }
+    }
+
+}
