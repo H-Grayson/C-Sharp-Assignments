840e657caec19b0087d930be1ce008030d8010b9
H-Grayson <92130199+H-Grayson@users.noreply.github.com>
2022-06-28 22:46:49 -0600

    .

---
+++ MethodSubmissionAssignment2/MethodSubmissionAssignment2/Methods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment2
+{
+    public class Methods
+    {
+        public static int Maths(int num1, int num2 = 9)
+        {
+            int result = num1 + 5 + num2;
+            return result;
+        }
+    }
+}
---
+++ MethodSubmissionAssignment2/MethodSubmissionAssignment2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods Maths = new Methods();
+
+            Console.WriteLine("Please enter a number: ");
+            string userNum1 = Console.ReadLine();
+
+
+            Console.WriteLine("Please enter a second number (Instructions say: Let them know they need not enter anything for the second number.) : ");
+            string userNum2 = Console.ReadLine();
+            int num2;
+
+            if (!int.TryParse(userNum2, out num2))
+            {
+                int num1 = Convert.ToInt32(userNum1);
+                Console.WriteLine("You have entered one number... Your Answer is : " + Methods.Maths(num1));
+            }
+
+            else
+            {
+                int num1 = Convert.ToInt32(userNum1);
+                num2 = Convert.ToInt32(userNum2);
+                Console.WriteLine("You have entered two numbers... Your Answer is : " + Methods.Maths(num1, num2));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
