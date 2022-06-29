24d67070d547044505df7426ce08937a2450d5d8
H-Grayson <92130199+H-Grayson@users.noreply.github.com>
2022-06-29 12:49:26 -0600

    Methods

---
+++ MethodsAssignment(OptParams)/MethodsAssignment(OptParams)/Methods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment_OptParams_
+{
+    public class Methods
+    {
+        public static int Maths(int num1, int num2 = 3)
+        {
+            int result = num1 + 10 + num2;
+            return result;
+        }
+    }
+}
---
+++ MethodsAssignment(OptParams)/MethodsAssignment(OptParams)/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment_OptParams_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods maths = new Methods();
+
+            Console.WriteLine("Please enter a number: ");
+            string userNum1 = Console.ReadLine();
+
+
+            Console.WriteLine("Please enter a second number - DO NOT HAVE TO Your Answer is : ");
+            string userNum2 = Console.ReadLine();
+
+            int num2;
+            if (!int.TryParse(userNum2, out num2))
+            {
+                int num1 = Convert.ToInt32(userNum1);
+                Console.WriteLine("You have entered one number. Your Answer is : " + Methods.Maths(num1));
+            }
+
+            else
+            {
+                int num1 = Convert.ToInt32(userNum1);
+                num2 = Convert.ToInt32(userNum2);
+                Console.WriteLine("You have entered two numbers. Your Answer is : " + Methods.Maths(num1, num2));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
