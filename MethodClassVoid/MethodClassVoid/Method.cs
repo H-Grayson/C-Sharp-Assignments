2c15fb31daab2238b5c60db605dc05f8506bf0c7
H-Grayson <92130199+H-Grayson@users.noreply.github.com>
2022-06-29 22:00:23 -0600

    MethodClassVoid Assignment

---
+++ MethodClassVoid/MethodClassVoid/Method.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassVoid
+{
+    public class Method
+    {
+        public static void voidMethod(int i1, int i2)
+        {
+            int b = i1 + 5;
+            Console.WriteLine(i2);
+            Console.ReadLine();
+        }
+    }
+} 
---
+++ MethodClassVoid/MethodClassVoid/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassVoid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method voidMethod = new Method();
+            Method.voidMethod(i1: 10, i2: 50);
+        }
+    }
+}
