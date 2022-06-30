764e1544f411e049889dc6a56a75e7245eb4b978
H-Grayson <92130199+H-Grayson@users.noreply.github.com>
2022-06-30 04:05:05 -0600

    VoidMethodAssignment2
    Overloading Methods in C#

---
+++ VoidMethodAssignment2/VoidMethodAssignment2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method newMethod = new Method();
+
+            Console.WriteLine("Input a number to be divided by 2");
+            int myNum1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please input a number to be multiplied by 2.");
+            int myNum2 = Convert.ToInt32(Console.ReadLine());
+
+            newMethod.methodDivision(myNum1);
+
+            newMethod.methodMultiplication(myNum2);
+
+
+
+            int outcome = newMethod.methodMultiplication(myNum1, myNum2);
+
+            Console.WriteLine("This is the result of your first number and your second number being multiplied by each other: {0}", outcome);
+
+            Console.Read();
+
+
+        }
+    }
+}
