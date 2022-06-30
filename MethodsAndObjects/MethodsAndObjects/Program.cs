1d274ebd963a8ed5ca3457893cd0cb8ec59ac761
H-Grayson <92130199+H-Grayson@users.noreply.github.com>
2022-06-30 05:38:22 -0600

    Methods and Objects Assignment

--- MethodsAndObjects/MethodsAndObjects/Program.cs
+++ MethodsAndObjects/MethodsAndObjects/Program.cs
@@ -10,6 +10,14 @@ namespace MethodsAndObjects
     {
         static void Main(string[] args)
         {
+            Employee emp = new Employee(); // Inside of the Main method, instantiate and initialize an Employee object
+
+            emp.FirstName = "Sample"; //  with a first name of “Sample” 
+            emp.LastName = "Student"; // and a last name of “Student”.
+            emp.Id = 1;
+
+            emp.SayName(); // Call the superclass method SayName() on the Employee object.
+            Console.ReadLine();
 
         }
     }
