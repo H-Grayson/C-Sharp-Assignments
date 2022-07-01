f61a1f2869dcd13c65f8755591ece7b5d4a627b3
H-Grayson <92130199+H-Grayson@users.noreply.github.com>
2022-07-01 00:51:58 -0600

    Operators Assignment

---
+++ OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    public class Employee //Create an Employee class with 
+    {
+        public int id { get; set; } // Id
+
+        public static bool operator ==(Employee emp1, Employee emp2) // In the Employee class, overload the “==” operator so it checks if two Employee objects are equal by comparing their Id property
+        {
+            return emp1.id == emp2.id;
+        }
+
+        public static bool operator !=(Employee emp1, Employee emp2) // == has to have a matching != for overloading operators
+        {
+            return emp1.id == emp2.id;
+        }
+
+        public string firstName { get; set; } // firstName property
+        public string lastName { get; set; } // lastName property
+
+    }
+}
