76bb7a59d83e03a9684b78fdaf7cfadbf9d68e5b
H-Grayson <92130199+H-Grayson@users.noreply.github.com>
2022-07-04 08:07:48 -0600

    Final Assignment

---
+++ FinalAssnment/FinalAssnment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FinalAssnment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Patrick" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+                Console.Write("Addition Successful!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
