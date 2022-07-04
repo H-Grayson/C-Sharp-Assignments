cbaa094c41a49902d209d8a7ca536c4ba8877822
H-Grayson <92130199+H-Grayson@users.noreply.github.com>
2022-07-04 06:43:59 -0600

    FinalChallenge

---
+++ FinalChallenge/FinalChallenge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Entity;
+
+
+
+namespace FinalChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var db = new SchoolContext())
+            { 
+                var stud = new Student() { StudentName = "Hunter" };
+
+                db.Students.Add(stud);
+                db.SaveChanges();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
